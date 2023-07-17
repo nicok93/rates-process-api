21b7365f8a14f9b7ecf7da4eb8df957371c5c238
Nicolás Kosacoff <nicokosacoff@gmail.com>
2023-07-17 01:09:17 -0300

    change variable

--- bin/install.sh
+++ bin/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Receiving token: "
-echo $1
-npm install --save git+https://$1:x-oauth-basic@github.com/nicok93/shipstation-system-api.git#main
-npm install --save git+https://$1:x-oauth-basic@github.com/nicok93/easypost-system-api.git#main
-npm install --save git+https://$1:x-oauth-basic@github.com/nicok93/sql-system-api.git#main
+echo $TOKEN
+npm install --save git+https://$TOKEN:x-oauth-basic@github.com/nicok93/shipstation-system-api.git#main
+npm install --save git+https://$TOKEN:x-oauth-basic@github.com/nicok93/easypost-system-api.git#main
+npm install --save git+https://$TOKEN:x-oauth-basic@github.com/nicok93/sql-system-api.git#main
