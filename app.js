2432609424ec2d98ec4d68c8d5d9c1dc41f902aa
Nicolás Kosacoff <nicokosacoff@gmail.com>
2023-07-20 23:44:46 -0300

    Start schedulers.

--- app.js
+++ app.js
@@ -5,5 +5,6 @@ app.use(express.json())
 import { properties } from './src/environments/properties.js';
 app.get('/', (req, res) => res.send('App is working'));
 app.use('/api', routes)
-
+import shipstation from 'shipstation-system-api';
+shipstation.schedulers.start();
 app.listen(process.env.PORT, () => console.log('Rates process API listening on port ' + properties.PORT + '!'))
