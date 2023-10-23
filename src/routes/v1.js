924f1f2f4f50dd29b853743cb5a261fb73d8d9c2
Nicolás Kosacoff <nicokosacoff@gmail.com>
2023-10-23 13:26:34 -0300

    Add label feature.

--- src/routes/v1.js
+++ src/routes/v1.js
@@ -4,6 +4,7 @@ import ordersController from '../controllers/orders.js';
 import storesController from '../controllers/stores.js';
 import warehousesController from '../controllers/warehouses.js';
 import productsController from '../controllers/products.js';
+import labelController from '../controllers/label.js';
 
 const routes = express.Router();
 
@@ -18,5 +19,7 @@ routes.get('/v1/stores/:id', storesController.getStore);
 routes.get('/v1/warehouses/:id', warehousesController.getWarehouse);
 
 routes.get('/v1/products', productsController.getProducts);
+
+routes.post('/v1/label', labelController.label);
 
 export { routes }
